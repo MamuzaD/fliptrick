20f1a5dc42a8a90094315fa49968b56eac1cce20
Daniel <94477022+MamuzaD@users.noreply.github.com>
2025-04-22 22:34:17 -0700

    spider-man

---
+++ AirborneTests/AirborneTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirborneTests.swift
+//  AirborneTests
+//
+//  Created by Daniel Mamuza on 4/22/25.
+//
+
+import Testing
+@testable import Airborne
+
+struct AirborneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
